656e62cff4ce04257c75d11de544c53bf95aff33
Mira-x77 <mira@gamtech-electronic.com>
2026-06-12 10:32:13 -0700

    Bust style.css cache using filemtime version

--- wp-content/themes/woodmart-child/functions.php
+++ wp-content/themes/woodmart-child/functions.php
@@ -3,7 +3,8 @@
  * Enqueue script and styles for child theme
  */
 function woodmart_child_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'woodmart-style' ), woodmart_get_theme_info( 'Version' ) );
+    $version = filemtime( get_stylesheet_directory() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'woodmart-style' ), $version );
 }
 add_action( 'wp_enqueue_scripts', 'woodmart_child_enqueue_styles', 10010 );
 
