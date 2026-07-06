29884713cfe9c43cff274513fe35a66ff3ef039f
mira <MJ.miracle.77@outlook.com>
2026-07-06 14:52:08 +0100

    Remove debug/recovery scripts

--- test-php.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-echo 'PHP works! Date: ' . date('Y-m-d H:i:s');
-echo "\nPHP version: " . phpversion();
-echo "\nABSPATH: " . (defined('ABSPATH') ? ABSPATH : 'not defined');
